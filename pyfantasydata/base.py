9d7888d3750d5092a349ebe99596b430f9390636
Nils Ballmann <nils.ballmann@gmail.com>
2018-01-29 23:59:07 +0100

    completely restructures base object

--- pyfantasydata/base.py
+++ pyfantasydata/base.py
@@ -3,7 +3,7 @@
 
 # pylint: disable=C0111
 
-__updated__ = '2018-01-29 22:36:55'
+__updated__ = '2018-01-29 23:52:36'
 
 import collections
 import xml.etree.ElementTree as et
@@ -11,7 +11,6 @@ import xml.etree.ElementTree as et
 import requests
 import yarl
 
-from . import scramble_filter as sf
 from . import enums
 
 
@@ -19,51 +18,56 @@ class Base(object):
     def __init__(
             self,
             api_key,
-            api_url=None,
-            api_version='v3',
-            api_response_format=enums.Format.JSON,
+            api_name,
+            api_category,
+            api_base_url=None,
+            api_response_format=None,
             **kwargs
         ): # yapf: disable
-        if isinstance(api_key, str):
+        if api_key is None:
+            raise ValueError('"api_key" must not be None')
+        elif isinstance(api_key, str):
             self.api_keys = [api_key]
         elif isinstance(api_key, collections.Iterable):
             self.api_keys = [str(key) for key in api_key]
         else:
             self.api_keys = [str(api_key)]
 
-        if isinstance(api_url, yarl.URL):
-            self.url = api_url
+        if isinstance(api_name, str):
+            self.api_name = enums.ApiName[api_name.upper()]
         else:
-            self.url = yarl.URL(
-                'https://api.fantasydata.net/{}/'.format(str(api_version))
-            )
+            self.api_name = enums.ApiName(api_name)
 
-        if isinstance(api_response_format, enums.Format):
-            self.api_response_format = api_response_format
+        if isinstance(api_name, str):
+            self.api_category = enums.ApiCategory[api_category.upper()]
         else:
-            self.api_response_format = enums.Format.JSON
+            self.api_category = enums.ApiCategory(api_category)
+
+        if api_base_url:
+            self.api_base_url = yarl.URL(api_base_url)
+        else:
+            self.api_base_url = yarl.URL('https://api.fantasydata.net/v3/')
+
+        if api_response_format:
+            self.api_response_format = enums.ApiResponseFormat(
+                api_response_format
+            )
+        else:
+            self.api_response_format = enums.ApiResponseFormat.JSON
 
         self.session = requests.Session(**kwargs)
 
-    def get(self, relative_url):
-        if isinstance(relative_url, str):
-            relative_url = yarl.URL(relative_url)
-        if not isinstance(relative_url, yarl.URL):
-            raise TypeError(
-                '"relative_url" must be of type "yarl.URL", but is of type "{}"'.
-                format(type(relative_url).__name__)
-            )
-        if relative_url.is_absolute():
-            raise ValueError(
-                '"relative_url" must be relative url, but is absolute url "{}"'.
-                format(relative_url)
-            )
+    def _url(self, endpoint):
+        endpoint = str(endpoint)
+        return self.api_base_url / self.api_name.name.lower(
+        ) / self.api_category.name.lower(
+        ) / self.api_response_format.name.lower() / endpoint
 
+    def _get(self, url, **kwargs):
         # try all the keys
         for key in self.api_keys:
             response = self.session.get(
-                self.url.join(relative_url),
-                headers={'Ocp-Apim-Subscription-Key': key}
+                url, headers={'Ocp-Apim-Subscription-Key': key}, **kwargs
             )
             if response.ok:
                 break
@@ -71,16 +75,11 @@ class Base(object):
             # exit if none was working
             raise SystemExit('No api key had success')
 
-        if self.api_response_format == enums.Format.JSON:
+        if self.api_response_format == enums.ApiResponseFormat.JSON:
             return response.json()
-        elif self.api_response_format == enums.Format.XML:
+        elif self.api_response_format == enums.ApiResponseFormat.XML:
             return et.fromstring(response.text)
 
-    def get_filtered(self, relative_url):
-        # filter out the free subscription scrambled data
-        result = self.get(relative_url)
-
-        if self.api_response_format == enums.Format.JSON:
-            return sf.filter_json(result)
-        elif self.api_response_format == enums.Format.XML:
-            return sf.filter_xml(result)
+    def get(self, endpoint, **kwargs):
+        url = self._url(endpoint)
+        return self._get(url, **kwargs)
