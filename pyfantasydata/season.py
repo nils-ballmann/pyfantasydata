cbff1c6e7825eef966011f26063e8cca2aff4d0d
Nils Ballmann <nils.ballmann@gmail.com>
2018-01-30 22:07:52 +0100

    corrected Season str order

--- pyfantasydata/season.py
+++ pyfantasydata/season.py
@@ -3,24 +3,24 @@
 
 # pylint: disable=C0111
 
-__updated__ = '2018-01-30 22:00:59'
+__updated__ = '2018-01-30 22:04:54'
 
 from . import enums
 
 
 class Season(object):
-    def __init__(self, season_type, year):
+    def __init__(self, year, season_type):
+        self.year = max(0, int(year))
+
         if isinstance(season_type, str):
             self.season_type = enums.SeasonType[season_type.upper()]
         else:
             self.season_type = enums.SeasonType(season_type)
 
-        self.year = max(0, int(year))
-
     def __str__(self):
-        return '{}{}'.format(self.season_type.name.upper(), self.year)
+        return '{}{}'.format(self.year, self.season_type.name.upper())
 
     def __repr__(self):
         return '{}({}, {})'.format(
-            self.__class__.__name__, self.season_type, repr(self.year)
+            self.__class__.__name__, repr(self.year), self.season_type
         )
